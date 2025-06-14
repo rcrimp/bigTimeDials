405af752cbc0425cd54fcf6fdf8939a9adf1cea6
Reuben Crimp <reubencrimp@Reubens-MacBook-Air.local>
2025-06-15 02:09:40 +1200

    refactor and space digits for other layouts

--- src/c/watchface.c
+++ src/c/watchface.c
@@ -8,10 +8,6 @@
 #define IMG_WIDTH 69
 #define IMG_HEIGHT 69
 
-#define DIGIT_PADDING 2
-#define SCREEN_WIDTH 144
-#define SCREEN_HEIGHT 168
-
 static Window *s_main_window;
 
 // fonts
@@ -89,57 +85,42 @@ static void main_window_load(Window *window) {
   s_image_numbers[8] = gbitmap_create_with_resource(RESOURCE_ID_EIGHT);
   s_image_numbers[9] = gbitmap_create_with_resource(RESOURCE_ID_NINE);
 
+  int screen_height = bounds.size.h;
+  int screen_width = bounds.size.w;
 
-  // Create BitmapLayer to display the GBitmap
-  s_time_digits[0] = bitmap_layer_create(GRect(DIGIT_PADDING, 0, IMG_WIDTH, IMG_HEIGHT));
-  bitmap_layer_set_bitmap(s_time_digits[0], s_image_numbers[0]);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_time_digits[0]));
-
-  s_time_digits[1] = bitmap_layer_create(GRect(IMG_WIDTH + DIGIT_PADDING * 2, 0, IMG_WIDTH, IMG_HEIGHT));
-  bitmap_layer_set_bitmap(s_time_digits[1], s_image_numbers[0]);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_time_digits[1]));
-
-
-  s_time_digits[2] = bitmap_layer_create(GRect(DIGIT_PADDING, SCREEN_HEIGHT - IMG_HEIGHT, IMG_WIDTH, IMG_HEIGHT));
-  bitmap_layer_set_bitmap(s_time_digits[2], s_image_numbers[0]);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_time_digits[2]));
-
-  s_time_digits[3] = bitmap_layer_create(GRect(IMG_WIDTH + DIGIT_PADDING * 2, SCREEN_HEIGHT - IMG_HEIGHT, IMG_WIDTH, IMG_HEIGHT));
-  bitmap_layer_set_bitmap(s_time_digits[3], s_image_numbers[0]);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_time_digits[3]));
+  GRect digits[4];
+  digits[0] = GRect(0, 0, IMG_WIDTH, IMG_HEIGHT);
+  digits[1] = GRect(screen_width - IMG_WIDTH, 0, IMG_WIDTH, IMG_HEIGHT);
+  digits[2] = GRect(0, screen_height - IMG_HEIGHT, IMG_WIDTH, IMG_HEIGHT);
+  digits[3] = GRect(screen_width - IMG_WIDTH, screen_height - IMG_HEIGHT, IMG_WIDTH, IMG_HEIGHT);
+  
+  for (int i = 0; i < DIGIT_COUNT; i++) {
+    s_time_digits[i] = bitmap_layer_create(digits[i]);
+    bitmap_layer_set_bitmap(s_time_digits[i], s_image_numbers[0]);
+    layer_add_child(window_layer, bitmap_layer_get_layer(s_time_digits[i]));
+  }
 
   // Create battery TextLayer
-  s_battery_layer = text_layer_create(
-    GRect(DIGIT_PADDING, 50, IMG_WIDTH, 14));
+  s_battery_layer = text_layer_create(GRect(0, 50, IMG_WIDTH, 14));
+  s_battery_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_RUBIK_14));
   text_layer_set_background_color(s_battery_layer, GColorClear);
   text_layer_set_text_color(s_battery_layer, GColorBlack);
   text_layer_set_text(s_battery_layer, "100%");
-  
-  s_battery_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_RUBIK_14));
-
   text_layer_set_font(s_battery_layer, s_battery_font);
   text_layer_set_text_alignment(s_battery_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_battery_layer));
 
-
-  
-  
   // Create date TextLayer
-  s_date_layer = text_layer_create(
-    GRect(0, IMG_HEIGHT, bounds.size.w, 24));
+  s_date_layer = text_layer_create(GRect(0, IMG_HEIGHT, bounds.size.w, 24));
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_RUBIK_24));
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorWhite);
   text_layer_set_text(s_date_layer, "Sat 14 Jun");
-  
-  // Create GFont
-  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_RUBIK_24));
-
-  // Apply to TextLayer
   text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 
-  // Make sure the time is displayed from the start
+  // update date and time on launch
   update_time();
   update_battery();
 }
